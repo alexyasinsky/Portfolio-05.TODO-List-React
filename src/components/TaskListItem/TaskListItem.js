0e0b0a0ebb996bf560a77d1c48bac7b5d7891d5b
Alex-region <alex_yasinsky@mail.com>
2022-11-21 17:43:35 +0300

    checking tasks in progress

--- src/components/TaskListItem/TaskListItem.js
+++ src/components/TaskListItem/TaskListItem.js
@@ -1,6 +1,6 @@
 import {CardContent, Typography, Card, Checkbox} from "@mui/material";
 import {useDispatch} from "react-redux";
-import {useCallback} from "react";
+import {useCallback, useState } from "react";
 import {setCurrentTask, setFormCase, toggleShowTaskForm} from "../../store/taskForm/actions";
 
 import './TaskListItem.scss';
@@ -9,16 +9,22 @@ export default function TaskListItem({task}) {
 
   const dispatch = useDispatch();
 
+  const [checked, setChecked] = useState(task.done);
+
   const handler = useCallback(()=> {
     dispatch(setFormCase('edit'));
     dispatch(toggleShowTaskForm());
     dispatch(setCurrentTask(task));
   }, [dispatch, task]);
 
+  function handleChecking (e) {
+    setChecked(e.target.checked);
+  };
+
   return(
     <Card className='taskListItem__card'>
       <CardContent className='taskListItem__content'>
-        <Checkbox/>
+        <Checkbox onChange={handleChecking} checked={checked}/>
         <div onClick={handler}>
           <Typography className='taskListItem__title'>
             {task.title}
